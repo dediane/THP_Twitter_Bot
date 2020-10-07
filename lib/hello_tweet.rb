28bac1984c695f0abcd33b2b345edb7738fe6d38
Diane Decourt <dianedecourt@gmail.com>
2020-10-07 14:21:29 +0200

    update

--- lib/hello_tweet.rb
+++ lib/hello_tweet.rb
@@ -3,11 +3,15 @@ require 'dotenv'
 
 Dotenv.load('.env')
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  return client
 end
 
-client.update('Mon premier tweet en Ruby !!!!')
+
+#client.update('Mon premier tweet en Ruby !!!!')
